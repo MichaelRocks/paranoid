9543c2de6570995cdbe56dafb45dfe28c58ab46b
Michael Rozumyanskiy <mr@pixty.com>
2019-06-04 11:28:37 +0300

    Cleanup outputs when the transform isn't incremental

--- gradle-plugin/src/main/java/io/michaelrocks/paranoid/plugin/ParanoidTransform.kt
+++ gradle-plugin/src/main/java/io/michaelrocks/paranoid/plugin/ParanoidTransform.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Michael Rozumyanskiy
+ * Copyright 2019 Michael Rozumyanskiy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,10 @@ class ParanoidTransform(
     private val android: BaseExtension
 ) : Transform() {
   override fun transform(invocation: TransformInvocation) {
+    if (!invocation.isIncremental) {
+      invocation.outputProvider.deleteAll()
+    }
+
     val inputs = invocation.inputs.flatMap { it.jarInputs + it.directoryInputs }
     val outputs = inputs.map { input ->
       val format = if (input is JarInput) Format.JAR else Format.DIRECTORY
