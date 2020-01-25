90c711dc53c5f9592a8438622b5d8d64c9b54eca
Michael Rozumyanskiy <michael.rozumyanskiy@gmail.com>
2020-01-25 13:11:16 +0300

    Replace Java code generation and compilation with bytecode generation, fixes #27

--- processor/src/main/kotlin/io/michaelrocks/paranoid/processor/StringRegistry.kt
+++ processor/src/main/kotlin/io/michaelrocks/paranoid/processor/StringRegistry.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Michael Rozumyanskiy
+ * Copyright 2020 Michael Rozumyanskiy
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,34 +16,45 @@
 
 package io.michaelrocks.paranoid.processor
 
-import java.util.HashMap
+import io.michaelrocks.paranoid.DeobfuscatorHelper
+import io.michaelrocks.paranoid.RandomHelper
+import java.security.SecureRandom
+import java.util.Random
 
 interface StringRegistry {
-  fun registerString(string: String): Int
-  fun getAllIds(): Collection<Int>
-  fun getAllStrings(): Collection<String>
-  fun findStringById(id: Int): String
+  fun registerString(string: String): Long
+  fun getAllChunks(): List<String>
 }
 
-class StringRegistryImpl : StringRegistry {
-  private val stringsById = HashMap<Int, String>()
-  private var lastId: Int = -1
+class StringRegistryImpl(
+  private val random: Random = SecureRandom()
+) : StringRegistry {
 
-  override fun registerString(string: String): Int {
-    val id = ++lastId
-    stringsById[id] = string
+  private val builder = StringBuilder()
+
+  override fun registerString(string: String): Long {
+    val seed = random.nextInt().toLong() and 0xffff_ffffL
+    var mask = 0L
+    var state = RandomHelper.seed(seed)
+    state = RandomHelper.next(state)
+    mask = mask or (state and 0xffff_0000_0000L)
+    state = RandomHelper.next(state)
+    mask = mask or ((state and 0xffff_0000_0000L) shl 16)
+    val index = builder.length
+    val id = seed or ((index.toLong() shl 32) xor mask)
+
+    state = RandomHelper.next(state)
+    builder.append((((state ushr 32) and 0xffffL) xor string.length.toLong()).toChar())
+
+    for (char in string) {
+      state = RandomHelper.next(state)
+      builder.append((((state ushr 32) and 0xffffL) xor char.toLong()).toChar())
+    }
+
     return id
   }
 
-  override fun getAllIds(): Collection<Int> {
-    return stringsById.keys.toList()
-  }
-
-  override fun getAllStrings(): Collection<String> {
-    return stringsById.values.toList()
-  }
-
-  override fun findStringById(id: Int): String {
-    return stringsById[id]!!
+  override fun getAllChunks(): List<String> {
+    return builder.toString().chunked(DeobfuscatorHelper.MAX_CHUNK_LENGTH)
   }
 }
